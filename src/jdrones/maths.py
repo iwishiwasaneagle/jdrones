99e65324a517e07a9feff84097b432bb6f2f1950
iwishiwasaneagle <jh.ewers@gmail.com>
2023-02-02 11:17:46 +0000

    feat: Move yaw and rpy transform calculations into maths module

--- src/jdrones/maths.py
+++ src/jdrones/maths.py
@@ -1,5 +1,7 @@
 import numpy as np
 import numpy.typing as npt
+from jdrones.transforms import euler_to_rotmat
+from jdrones.types import VEC3
 
 
 def clip_scalar(value: float, vmin: float, vmax: float) -> float:
@@ -8,3 +10,34 @@ def clip_scalar(value: float, vmin: float, vmax: float) -> float:
 
 def clip(value: npt.ArrayLike, vmin: float, vmax: float) -> npt.ArrayLike:
     return np.core.umath.maximum(np.core.umath.minimum(value, vmax), vmin)
+
+
+def yaw(x1: float, y1: float, x2: float, y2: float) -> float:
+    """
+
+    Parameters
+    ----------
+    x1 : float
+        From :math:`x`
+    y1 : float
+        From :math:`y`
+    x2 : float
+        To :math:`x`
+    y2 : float
+        To :math:`y`
+
+    Returns
+    -------
+    float
+        :math:`\\psi` (rad)
+
+    """
+    return np.arctan2(y2 - y1, x2 - x1)
+
+
+def euclidean_distance(x1: float, y1: float, x2: float, y2: float) -> float:
+    return np.sqrt(np.sum(np.square([x2 - x1, y2 - y1])))
+
+
+def apply_rpy(value: VEC3, rpy: VEC3) -> VEC3:
+    return np.dot(value, euler_to_rotmat(rpy))
--- tests/test_maths.py
+++ tests/test_maths.py
@@ -1,8 +1,11 @@
 import numpy as np
+import pytest
 from hypothesis import given
 from hypothesis import strategies as st
+from jdrones.maths import apply_rpy
 from jdrones.maths import clip
 from jdrones.maths import clip_scalar
+from jdrones.maths import yaw
 
 
 @given(
@@ -23,3 +26,38 @@ def test_clip_scalar(value, vmin, vmax):
 def test_clip_array(value, vmin, vmax):
     clipped = clip(value, vmin, vmax)
     assert np.all((vmin <= np.asarray(clipped)) <= vmax)
+
+
+@pytest.mark.parametrize(
+    "a,b,exp",
+    [
+        [(0, 0), (1, 0), 0],
+        [(0, 0), (0, 1), np.pi / 2],
+        [(0, 0), (0, -1), -np.pi / 2],
+        [(0, 0), (-1, 0.0000001), np.pi],
+        [(0, 0), (-1, -0.0000001), -np.pi],
+        [(100, 50), (100, 70), np.pi / 2],
+        [(1, 2), (2, 4), 1.1071487177940904],
+    ],
+)
+def test_yaw(a, b, exp):
+    act = yaw(*a, *b)
+    assert np.isclose(act, exp)
+
+
+@pytest.mark.parametrize(
+    "value,rpy,exp",
+    [
+        [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
+        [(1, 0, 0), (0, 0, 0), (1, 0, 0)],
+        [(0, 1, 0), (0, 0, 0), (0, 1, 0)],
+        [(0, 0, 1), (0, 0, 0), (0, 0, 1)],
+        [(1, 0, 0), (0, 0, np.pi / 2), (0, -1, 0)],
+        [(0, 1, 0), (0, 0, np.pi / 2), (1, 0, 0)],
+        [(0, 0, 1), (0, 0, np.pi / 2), (0, 0, 1)],
+        [(1, 1, 0), (np.pi / 2, 0, 0), (1, 0, -1)],
+    ],
+)
+def test_apply_rpy(value, rpy, exp):
+    act = apply_rpy(value, rpy)
+    assert np.allclose(act, exp)
