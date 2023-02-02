7860f19cd6a1a85fb6fb37f94ed62d93fedd4b82
iwishiwasaneagle <jh.ewers@gmail.com>
2023-02-02 14:35:10 +0000

    chore: Export DronePlus from envs

--- src/jdrones/envs/__init__.py
+++ src/jdrones/envs/__init__.py
@@ -1,5 +1,6 @@
 from .attitude import AttitudeAltitudeDroneEnv
 from .base import BaseDroneEnv
 from .drone import DroneEnv
+from .dronemodels import DronePlus
 from .trajectory import PIDTrajectoryDroneEnv
 from .velocityheading import VelHeadAltDroneEnv
