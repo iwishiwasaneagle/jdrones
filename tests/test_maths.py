083e53fc04a9dc270df96f108c8615e3f50e9ba3
iwishiwasaneagle <jh.ewers@gmail.com>
2023-03-01 15:37:39 +0000

    feat: Quaternion multiplication

--- tests/test_maths.py
+++ tests/test_maths.py
@@ -2,12 +2,15 @@
 #  SPDX-License-Identifier: GPL-3.0-only
 import numpy as np
 import pytest
+from hypothesis import assume
 from hypothesis import given
 from hypothesis import strategies as st
 from jdrones.maths import apply_rpy
 from jdrones.maths import clip
 from jdrones.maths import clip_scalar
+from jdrones.maths import quat_mul
 from jdrones.maths import yaw
+from scipy.spatial.transform import Rotation as R
 
 
 @given(
@@ -62,4 +65,29 @@ def test_yaw(a, b, exp):
 )
 def test_apply_rpy(value, rpy, exp):
     act = apply_rpy(value, rpy)
+    assert np.allclose(act, exp)
+
+
+NonNanNonInfFloat = st.floats(
+    allow_nan=False,
+    allow_subnormal=False,
+    allow_infinity=False,
+    min_value=-1,
+    max_value=1,
+)
+QUAT = st.tuples(*(NonNanNonInfFloat,) * 4)
+
+
+@given(
+    a=QUAT,
+    b=QUAT,
+)
+def test_quat_mul(a, b):
+    assume(np.sum(a) != 0 and np.sum(b) != 0)
+
+    ar = R.from_quat(a)
+    br = R.from_quat(b)
+
+    act = quat_mul(ar.as_quat(), br.as_quat())
+    exp = (ar * br).as_quat()
     assert np.allclose(act, exp)
