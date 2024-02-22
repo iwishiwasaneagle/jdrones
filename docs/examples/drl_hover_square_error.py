675947e6ca17b0f7b390535305794e2f60820d63
iwishiwasaneagle <me@janhendrikewers.uk>
2024-02-22 09:31:24 +0000

    feat: DRL controlled drone going from A to hovering at B

---
+++ docs/examples/drl_hover_square_error.py
@@ -0,0 +1,75 @@
+"""
+A simple example of how to use SB3's PPO algorithm to control a drone from A to B
+and then to hover there until time
+runs out. This uses a square error reward function.
+
+Run
+===
+
+..code-block:: bash
+    PYTHONPATH=src python docs/examples/drl_hover_square_error.py
+
+"""
+from typing import Optional
+from typing import Tuple
+
+import gymnasium
+import numpy as np
+from gymnasium.wrappers import TimeLimit
+from jdrones.data_models import State
+from jdrones.envs import NonlinearDynamicModelDroneEnv
+from jdrones.types import PropellerAction
+from loguru import logger
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.monitor import Monitor
+
+logger.info(f"Starting {__file__}")
+
+
+class HoverMSERewardWrapperNonlinearDynamicModelDroneEnv(NonlinearDynamicModelDroneEnv):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        high, low = self.observation_space.high, self.observation_space.low
+        high[:3] = 10
+        low[:3] = -10
+        self.observation_space = gymnasium.spaces.Box(
+            low=low, high=high, dtype=self.observation_space.dtype
+        )
+        self.action_space = gymnasium.spaces.Box(
+            low=np.zeros(4), high=np.full(4, 1.0), dtype=self.action_space.dtype
+        )
+
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        options: Optional[dict] = None,
+    ) -> Tuple[State, dict]:
+        self.hover_tgt = self.observation_space.sample()[:3]
+        return super().reset(seed=seed, options=options)
+
+    def step(self, action: PropellerAction) -> Tuple[State, float, bool, bool, dict]:
+        obs, _, trunc, term, info = super().step(action)
+        distance_from_tgt = np.linalg.norm(self.hover_tgt - self.state.pos)
+        reward = -np.square(distance_from_tgt)
+
+        return obs, reward, trunc, term, info
+
+
+def make_env():
+    env = HoverMSERewardWrapperNonlinearDynamicModelDroneEnv()
+    env = TimeLimit(env, int(10 / env.dt))
+    env = Monitor(env)
+    return env
+
+
+env = make_vec_env(make_env, n_envs=1)
+eval_env = make_vec_env(make_env, n_envs=10)
+model = PPO("MlpPolicy", env=env, verbose=0, tensorboard_log="/tmp/jdrones")
+eval_callback = EvalCallback(
+    eval_env, eval_freq=50000, n_eval_episodes=10, deterministic=True, render=False
+)
+model.learn(total_timesteps=1e7, progress_bar=True, callback=eval_callback)
